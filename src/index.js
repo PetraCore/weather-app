e4616ca9bb8b9e4f6bdbf598ae475baf9861108a
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-05-21 15:43:21 +0200

    Remove template placeholders

--- src/index.js
+++ src/index.js
@@ -1,8 +1,2 @@
 import "normalize.css";
 import "./style.css";
-import Cat from "./cat.jpg";
-
-console.log("Hello, world!");
-const catImage = new Image();
-catImage.src = Cat;
-document.querySelector("body").appendChild(catImage);
