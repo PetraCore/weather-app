8cecb4311ef7cce4bd56467e98ad11a92e99c247
PetraCore <102828971+PetraCore@users.noreply.github.com>
2024-05-27 14:02:11 +0200

    Add ability to search weather

--- src/modules/composer.js
+++ src/modules/composer.js
@@ -10,15 +10,36 @@ export default class Composer {
     pref_meters: true,
   };
 
+  clearWeather() {
+    this.#weatherContainer.innerHTML = "";
+  }
+
   async displayWeather(query) {
     const forecast = await this.#weather.fetchData(query);
     console.log(forecast);
 
     const weatherCard = this.#painter.createWeatherCard(forecast, this.#locale);
+    this.clearWeather();
     this.#weatherContainer.appendChild(weatherCard);
   }
 
+  activateSearchBox() {
+    const search = document.querySelector("#search");
+    const searchButton = document.querySelector("#searchButton");
+
+    search.addEventListener("keydown", ({ key }) => {
+      if (key === "Enter") {
+        this.displayWeather(search.value);
+      }
+    });
+
+    searchButton.addEventListener("click", () => {
+      this.displayWeather(search.value);
+    });
+  }
+
   async initApp() {
+    this.activateSearchBox();
     this.displayWeather("London");
   }
 }
